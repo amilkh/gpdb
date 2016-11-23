a8c3cdb1c052210ef88562787ce77c5b497b1138
Jimmy Yih <jyih@pivotal.io>
2016-11-23 15:59:14 -0800

    Remove autogenerated sql files from uao_catalog_tabs TINC test

--- src/test/tinc/tincrepo/mpp/gpdb/tests/storage/uao/uao_catalog_tabs/sql/deletetablevisimapinfo_06.sql
+++
@@ -1,6 +0,0 @@
-select * from uao_visimap_test06;
-select count(*) from gp_dist_random('pg_aoseg.pg_aovisimap_18663');
-insert into uao_visimap_test06 select i,'aa'||i,i+10 from generate_series(1,5) as i;
-delete from uao_visimap_test06 where i=3;
-select * from uao_visimap_test06;
-select count(*) from gp_dist_random('pg_aoseg.pg_aovisimap_18663');
--- src/test/tinc/tincrepo/mpp/gpdb/tests/storage/uao/uao_catalog_tabs/sql/truncatevisimapinfo_05.sql
+++
@@ -1,9 +0,0 @@
-select * from uao_visimap_test05;
-select count(*) from gp_dist_random('pg_aoseg.pg_aovisimap_18636');
-insert into uao_visimap_test05 select i,'aa'||i,i+10 from generate_series(1,5) as i;
-delete from uao_visimap_test05 where i=3;
-select * from uao_visimap_test05;
-select count(*) from gp_dist_random('pg_aoseg.pg_aovisimap_18636');
-truncate table uao_visimap_test05;
-select * from uao_visimap_test05;
-select count(*) from gp_dist_random('pg_aoseg.pg_aovisimap_18636');
